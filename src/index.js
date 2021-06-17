f9551e80398cac23e829f83ea76ad7be647a8dc8
Gifairu <gifairu@gmail.com>
2021-06-17 17:17:08 +0800

    Adding Quotes

--- src/index.js
+++ src/index.js
@@ -1,24 +1,5 @@
 const Card = require('./scripts/Card.js');
 const Instagram = require('./scripts/Instagram.js');
+const Quotes = require('./scripts/Quotes.js');
 
-/** Card Example
- * @type {name}
- * @type {position}
- * @type {facebook}
- * @type {instagram}
- * @type {discord}
- * @type {avatar}
- */
-Card(
-	'Gifairu Xist',
-	'Front-End Developer',
-	'Gifairu (Xist)',
-	'Gifairu',
-	'Gifairu#0001',
-	'https://github.com/Gifairu.png?size=460'
-);
-
-/** Instagram Example
- * @type {username}
- */
-Instagram('discord');
+module.exports = { Card, Instagram, Quotes };
